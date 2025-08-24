cc49d4db6c2b6dbf5e3d30c2f60cef911f8c267e
Katherine Delgado <ksdelgado31@gmail.com>
2025-08-24 01:26:34 -0600

    Creating a navbar. Adding daisyUI.

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World wit stimulus y hola!"
+    this.element.textContent = "Hello World with stimulus y hola mundo!"
   }
 }
