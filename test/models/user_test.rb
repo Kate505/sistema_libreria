7532a37d9d8115797cf3853cdb3ade0e1be26e23
Katherine Delgado <ksdelgado31@gmail.com>
2026-03-01 01:27:36 -0600

    feat: add employee and session fixtures, enhance user tests with validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user fixture is valid" do
+    user = users(:one)
+    assert user.valid?, "users(:one) debería ser válido"
+  end
+
+  test "user belongs to empleado" do
+    user = users(:one)
+    assert_not_nil user.empleado, "El usuario debería tener un empleado asociado"
+  end
+
+  test "email address is normalized to lowercase" do
+    user = users(:one)
+    assert_equal user.email_address, user.email_address.downcase
+  end
 end
